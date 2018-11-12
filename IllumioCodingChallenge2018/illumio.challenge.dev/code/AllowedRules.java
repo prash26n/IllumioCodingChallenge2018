6d4d287177205b6048143d002ee8a19fc738d704
prash26n <prashant09033@iiitd.ac.in>
2018-11-12 04:24:42 -0500

    Added - AllowedRules.java a java bean with properties of an allowed rule in the network.
    Firewall.java the main class for creating the allowed rules based on an
    incoming csv file.
    
    networkrules.csv - test input file

--- IllumioCodingChallenge2018/illumio.challenge.dev/code/AllowedRules.java
+++ IllumioCodingChallenge2018/illumio.challenge.dev/code/AllowedRules.java
@@ -4,10 +4,69 @@ package code;
  * 
  * @author Prashant Singh
  * 
- * init
+ * This class contains the different fields for a valid Firewall rule that is allowed in the network. 
+ * A rule is equal to the other rule only if all of direction, protocol, port and IP Address match for that rule.
  *
  */
 
 public class AllowedRules {
 
+	 private String direction;
+	 private String protocol;
+	 private int port;
+	 private long ipAddress;
+
+
+     public AllowedRules(String direction, String protocol, String port, String ipAddress) {
+         this.direction = direction;
+         this.protocol = protocol;
+         this.port = Integer.parseInt(port);
+         this.ipAddress = Long.parseLong(ipAddress.replaceAll("\\.", "")); 
+     }
+
+     public AllowedRules(String direction, String protocol, String port, long ipAddress) {
+         this.direction = direction;
+         this.protocol = protocol;
+         this.port = Integer.parseInt(port);
+         this.ipAddress = ipAddress;
+     }
+
+     public AllowedRules(String direction, String protocol, int port, long ipAddress) {
+         this.direction = direction;
+         this.protocol = protocol;
+         this.port = port;
+         this.ipAddress = ipAddress;
+     }
+
+     public AllowedRules(String direction, String protocol, int port, String ipAddress) {
+         this.direction = direction;
+         this.protocol = protocol;
+         this.port = port;
+         this.ipAddress = Long.parseLong(ipAddress.replaceAll("\\.", "")); //convert string ipAddress with periods to just a number
+     }
+     
+     @Override
+     public boolean equals(Object object) {
+         if (this == object) return true;
+         
+         if (!(object instanceof AllowedRules)) return false;
+         
+         AllowedRules networkRule = (AllowedRules) object;
+         
+         //return true if all conditions are satisfied
+         return  direction.equalsIgnoreCase(networkRule.direction) && protocol.equalsIgnoreCase(networkRule.protocol)
+        		 && port == networkRule.port && ipAddress == networkRule.ipAddress;
+     }
+
+     @Override
+     public String toString() {
+         return this.direction +  ", " + this.protocol + ", " + Integer.toString(this.port) + ", " + Long.toString(this.ipAddress);
+     }
+
+
+     public int hashCode() {
+         long hash =  99 * (this.ipAddress + this.port + this.direction.hashCode() + this.protocol.hashCode()); //get unique key from all the components
+         return Long.valueOf(hash).hashCode();
+     }
+     
 }
--- IllumioCodingChallenge2018/illumio.challenge.dev/code/Firewall.java
+++ IllumioCodingChallenge2018/illumio.challenge.dev/code/Firewall.java
@@ -1,13 +1,120 @@
 package code;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+
 /**
  * 
  * @author Prashant Singh
  * 
- * init
+ * This class is the actual Firewall. It constructs an object based on the rules defined in the networkrules.csv 
+ * 
  *
  */
 
 public class Firewall {
 
+	//A hashmap to store an allowed rule and an Integer to check validity of the rule.
+	
+		static HashMap<AllowedRules,Integer> allowedRules = new HashMap<AllowedRules,Integer>();
+
+		public Firewall(String filename){
+
+			try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
+
+				String line;
+				while((line = br.readLine()) != null) {
+
+					String [] rule = line.split(",");
+
+					/* When rule has range of ports*/
+					if (rule[2].contains("-")) { 
+						String [] portRanges = rule[2].split("-");
+						int portMin = Integer.parseInt(portRanges[0]);
+						int portMax = Integer.parseInt(portRanges[1]);
+						int portRange = portMax - portMin;
+
+						/* When rule has range in octet of ip address */
+						if (rule[3].contains("-")) {
+							String [] ipRange = rule[3].split("-");
+							long ipAddressMin = Long.parseLong(ipRange[0].replaceAll("\\.", ""));
+							long ipAddressMax = Long.parseLong(ipRange[1].replaceAll("\\.", ""));
+							long totalIP = ipAddressMax - ipAddressMin;
+
+							//iterate through all possible ports and ips and add them to set
+							for (int i = 0; i <= portRange; i++) {
+								for (int j = 0; j <= totalIP; j++) {
+									AllowedRules currRule = new AllowedRules(rule[0], rule[1], portMin + i, ipAddressMin + j);
+									allowedRules.put(currRule,1);
+								}
+							}
+
+							//iterate through all possible ports and ips and add them to set
+							for (int i = 0; i <= portRange; i++) {
+								for (int j = 0; j <= totalIP; j++) {
+									AllowedRules currRule = new AllowedRules(rule[0], rule[1], portMin + i, ipAddressMin + j);
+									allowedRules.put(currRule,1);
+								}
+							}
+						}else{
+
+							//iterate through all possible ports add them to mssetap
+							for (int i = 0; i <= portRange; i++) {
+								AllowedRules currRule = new AllowedRules(rule[0],rule[1], portMin + i, rule[3]);
+								allowedRules.put(currRule,1);
+							}
+						}
+					}  
+
+					else /* When rule has no range of ports*/
+
+					{ 
+						/* When rule has  range of ip address*/
+						if (rule[3].contains("-")) {
+							String [] ipRange = rule[3].split("-");
+							long ipAddressMin = Long.parseLong(ipRange[0].replaceAll("\\.", ""));
+							long ipAddressMax = Long.parseLong(ipRange[1].replaceAll("\\.", ""));
+							long totalIP = ipAddressMax - ipAddressMin;
+
+							//iterate through all possible ips add them to map
+							for (int i = 0; i <= totalIP; i++) {
+								AllowedRules currRule = new AllowedRules(rule[0],rule[1],rule[2], ipAddressMin + i);
+								allowedRules.put(currRule,1);
+							}
+						}
+						else 
+							/* When rule has  no range of ip address*/
+						{ 
+							AllowedRules currRule = new AllowedRules(rule[0],rule[1],rule[2],rule[3]);
+							allowedRules.put(currRule,1);
+						}
+
+					}
+
+				}
+			}
+			catch (FileNotFoundException e) {
+				System.out.println("Unable to find the specified file "+ filename );
+			}
+			catch(Exception e) {
+				System.out.println("Exeption occured " + e.getMessage());
+			}
+		}
+
+		/**
+		 * This function is used to check the validity of an incoming netowrk configuration.
+		*/
+		
+		public boolean acceptPacket(String direction, String protocol, int port, String ipAddress) {
+			AllowedRules rule = new AllowedRules(direction, protocol, port, ipAddress);
+			if (allowedRules.containsKey(rule)) {
+				return true;
+			}
+			else {
+				return false;
+			}
+
+		}
 }
